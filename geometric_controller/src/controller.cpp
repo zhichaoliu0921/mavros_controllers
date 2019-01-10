b4ebdbcfe3d45d965de16a280c0d107ce176998e
JaeyoungLim <gokoreas@gmail.com>
2019-01-10 13:11:39 +0100

    Add controller object

---
+++ geometric_controller/src/controller.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jalim on 10.01.19.
+//
+
+#include "geometric_controller/controller.h"
+
+controller::controller(){
+
+}
+
+trajectory::~trajectory(){
+
+};
---
+++ geometric_controller/src/controller.h
@@ -0,0 +1,20 @@
+//
+// Created by jalim on 10.01.19.
+//
+
+#ifndef GEOMETRIC_CONTROLLER_CONTROLLER_H
+#define GEOMETRIC_CONTROLLER_CONTROLLER_H
+
+#include <Eigen/Dense>
+
+class controller {
+private:
+
+public:
+    controller();
+    ~controller();
+};
+
+#endif //GEOMETRIC_CONTROLLER_CONTROLLER_H
+
+
