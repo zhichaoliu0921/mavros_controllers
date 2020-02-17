b109a69af50d33f43fe049bf0be68d7020dcf3c1
zhichaoliu0921 <zliu157@ucr.edu>
2020-02-17 13:36:36 -0800

    calib q_des

--- geometric_controller/src/geometric_controller.cpp
+++ geometric_controller/src/geometric_controller.cpp
@@ -251,7 +251,7 @@ void geometricCtrl::statusloopCallback(const ros::TimerEvent& event){
     }
   }
   pubSystemStatus();
-    ROS_INFO("current q_des x  = %f, y = %f, z = %f, w =%f", q_des(0),q_des(1),q_des(2), q_des(3));
+    //ROS_INFO("current q_des x  = %f, y = %f, z = %f, w =%f", q_des(0),q_des(1),q_des(2), q_des(3));
 }
 
 void geometricCtrl::pubReferencePose(const Eigen::Vector3d &target_position, const Eigen::Vector4d &target_attitude){
@@ -336,6 +336,7 @@ void geometricCtrl::computeBodyRateCmd(Eigen::Vector4d &bodyrate_cmd){
   }
 
   const Eigen::Vector4d q_ref = acc2quaternion(a_ref - g_, mavYaw_);
+  ROS_INFO("current q_ref x  = %f, y = %f, z = %f, w =%f", q_ref(0),q_ref(1),q_ref(2), q_ref(3));
   const Eigen::Matrix3d R_ref = quat2RotMatrix(q_ref);
 
   const Eigen::Vector3d pos_error = mavPos_ - targetPos_;
